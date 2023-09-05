d8fc55eb417fc687fa9fc7d6e66347889ae99183
ryosuke-shibata-hub <ibis.stk@gmail.com>
2023-09-05 23:46:51 +0900

    todoアプリはできた次はデプロイ

--- reactApp/app/src/App.js
+++ reactApp/app/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Form, InputGroup, Button} from "react-bootstrap";
+import { Container, Form, InputGroup, Button, Table} from "react-bootstrap";
 
 function App() {
     const [value, setValue] = useState("")
@@ -13,7 +13,21 @@ function App() {
     }
 
     const addTodo = text => {
-        const newTodos = [...todos, text]
+        const newTodos = [...todos, { text, complete: false }]
+        setTodos(newTodos)
+        setValue("")
+        console.log(newTodos);
+    }
+
+    const removeTodo = index => {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
+
+    const completeTodo = index => {
+        const newTodos = [...todos]
+        newTodos[index].complete = !newTodos[index].complete
         setTodos(newTodos)
         console.log(newTodos);
     }
@@ -28,6 +42,29 @@ function App() {
                         <Button type="submit" className="btn btn-primary">追加</Button>
                     </InputGroup>
                 </Form>
+            </Container>
+            <Container>
+                <Table>
+                    <tbody>
+                        {todos && todos.map((todo, index) => (
+                            <tr key={index}>
+                                <th className="text-left" style={{ textDecoration: todo.complete ? "line-through" : "" }}>{todo.text}</th>
+                                <td className="text-end">
+                                    <Button
+                                        className={todo.complete ? "btn btn-success me-2" : "btn btn-succsess me-2"}
+                                        onClick={() => completeTodo(index)}>
+                                        {todo.complete ? '完了' : '未完了'}
+                                    </Button>
+                                    <Button
+                                        className="btn btn-danger"
+                                        onClick={() => removeTodo(index)}>
+                                        削除
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
             </Container>
         </div>
     );
